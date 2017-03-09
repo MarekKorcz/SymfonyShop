6d5c8974189636f0463167948ecc1c73b0f5767c
Marek Korcz <mark.korcz@gmail.com>
2017-03-09 23:10:19 +0100

    Creating User entity + plugging in FOSUserBundle

---
+++ src/ShopBundle/Entity/User.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace ShopBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="fos_user")
+ * @ORM\Entity(repositoryClass="ShopBundle\Repository\UserRepository")
+ */
+class User extends BaseUser
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+    
+    public function __construct(){
+
+    parent:: __construct();
+
+    }
+}
