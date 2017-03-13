dbf4b9641b552c03883b33391e18c15efae9bf8c
Marek Korcz <mark.korcz@gmail.com>
2017-03-13 22:12:30 +0100

    Generating CRUD for Product entity

---
+++ src/ShopBundle/Form/ProductType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace ShopBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProductType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name')->add('description')->add('price')->add('category');
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'ShopBundle\Entity\Product'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'shopbundle_product';
+    }
+
+
+}
