ce5ccf407f527b47d50a57061df1cf75449695c5
Marek Korcz <mark.korcz@gmail.com>
2017-03-13 22:16:01 +0100

    Generating CRUD for UserOrder entity

--- src/ShopBundle/Controller/UserOrderController.php
+++ src/ShopBundle/Controller/UserOrderController.php
@@ -2,9 +2,135 @@
 
 namespace ShopBundle\Controller;
 
+use ShopBundle\Entity\UserOrder;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;use Symfony\Component\HttpFoundation\Request;
 
-class OrderController extends Controller
+/**
+ * Userorder controller.
+ *
+ * @Route("order")
+ */
+class UserOrderController extends Controller
 {
+    /**
+     * Lists all userOrder entities.
+     *
+     * @Route("/", name="order_index")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $userOrders = $em->getRepository('ShopBundle:UserOrder')->findAll();
+
+        return $this->render('userorder/index.html.twig', array(
+            'userOrders' => $userOrders,
+        ));
+    }
+
+    /**
+     * Creates a new userOrder entity.
+     *
+     * @Route("/new", name="order_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request)
+    {
+        $userOrder = new Userorder();
+        $form = $this->createForm('ShopBundle\Form\UserOrderType', $userOrder);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($userOrder);
+            $em->flush($userOrder);
+
+            return $this->redirectToRoute('order_show', array('id' => $userOrder->getId()));
+        }
+
+        return $this->render('userorder/new.html.twig', array(
+            'userOrder' => $userOrder,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a userOrder entity.
+     *
+     * @Route("/{id}", name="order_show")
+     * @Method("GET")
+     */
+    public function showAction(UserOrder $userOrder)
+    {
+        $deleteForm = $this->createDeleteForm($userOrder);
+
+        return $this->render('userorder/show.html.twig', array(
+            'userOrder' => $userOrder,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing userOrder entity.
+     *
+     * @Route("/{id}/edit", name="order_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, UserOrder $userOrder)
+    {
+        $deleteForm = $this->createDeleteForm($userOrder);
+        $editForm = $this->createForm('ShopBundle\Form\UserOrderType', $userOrder);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('order_edit', array('id' => $userOrder->getId()));
+        }
+
+        return $this->render('userorder/edit.html.twig', array(
+            'userOrder' => $userOrder,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a userOrder entity.
+     *
+     * @Route("/{id}", name="order_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, UserOrder $userOrder)
+    {
+        $form = $this->createDeleteForm($userOrder);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($userOrder);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('order_index');
+    }
+
+    /**
+     * Creates a form to delete a userOrder entity.
+     *
+     * @param UserOrder $userOrder The userOrder entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(UserOrder $userOrder)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('order_delete', array('id' => $userOrder->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
 }
---
+++ src/ShopBundle/Form/UserOrderType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace ShopBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserOrderType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('status')->add('date')->add('user');
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'ShopBundle\Entity\UserOrder'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'shopbundle_userorder';
+    }
+
+
+}
