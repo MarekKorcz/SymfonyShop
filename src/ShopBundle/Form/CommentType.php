123b9d1ec18cdbdde8768741bd0aca65a500e806
Marek Korcz <mark.korcz@gmail.com>
2017-03-13 22:07:34 +0100

    Generating CRUD for Comment entity

---
+++ src/ShopBundle/Form/CommentType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace ShopBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CommentType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('text')->add('date')->add('user')->add('product');
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'ShopBundle\Entity\Comment'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'shopbundle_comment';
+    }
+
+
+}
