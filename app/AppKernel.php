c58fae9c9befcea11b27b429dce880ac31703307
Marek Korcz <mark.korcz@gmail.com>
2017-03-26 21:57:32 +0200

    Creating new bundle and controller in order to override default FOSUserBundle's register method

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new ShopBundle\ShopBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new UserBundle\UserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
