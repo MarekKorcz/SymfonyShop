d33faabaa00914dc77ca1b2282f61237ff13b19f
Marek Korcz <mark.korcz@gmail.com>
2017-03-31 23:31:18 +0200

    Adding my very first admin class (CategoryAdmin)

---
+++ src/ShopBundle/Admin/CategoryAdmin.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace ShopBundle\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Show\ShowMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+
+class CategoryAdmin extends AbstractAdmin
+{
+    // Fields to be shown on create/edit forms
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('name', 'text', array(
+                'label' => 'Category name'
+            ))
+            ->add('section', 'entity', array(
+                'class' => 'ShopBundle\Entity\Section'
+            ))
+            ->add('products', 'entity', array(
+                'class' => 'ShopBundle\Entity\Product'
+            ))
+        ;
+    }
+
+    // Fields to be shown on filter forms
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+       $datagridMapper
+            ->add('name')
+            ->add('section')
+            ->add('products')
+       ;
+    }
+
+    // Fields to be shown on lists
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('name')
+            ->add('section')
+            ->add('products')
+       ;
+    }
+
+    // Fields to be shown on show action
+    protected function configureShowFields(ShowMapper $showMapper)
+    {
+        $showMapper
+           ->add('name')
+           ->add('section')
+           ->add('products')
+       ;
+    }
+}
