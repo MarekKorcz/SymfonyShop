093f72af2535561f1cdbb288da1d2bd8f3797b64
Marek Korcz <mark.korcz@gmail.com>
2017-04-03 22:49:46 +0200

    Adding Section entity to admins dashboard

---
+++ src/ShopBundle/Admin/SectionAdmin.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace ShopBundle\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Show\ShowMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+
+class SectionAdmin extends AbstractAdmin
+{
+    // Fields to be shown on create/edit forms
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('name', 'text', array(
+                'label' => 'Section name'
+            ))
+            ->add('categories', 'entity', array(
+                'class' => 'ShopBundle\Entity\Category'
+            ))
+        ;
+    }
+
+    // Fields to be shown on filter forms
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+       $datagridMapper
+            ->add('name')
+            ->add('categories')
+       ;
+    }
+
+    // Fields to be shown on lists
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('name')
+            ->add('categories')
+       ;
+    }
+
+    // Fields to be shown on show action
+    protected function configureShowFields(ShowMapper $showMapper)
+    {
+        $showMapper
+           ->add('name')
+           ->add('categories')
+       ;
+    }
+}
