ba737a9efc2dd63eb70d1f0a85c6f25581745727
Marek Korcz <mark.korcz@gmail.com>
2017-04-08 21:19:28 +0200

    Enabling the new bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -23,6 +23,7 @@ class AppKernel extends Kernel
             new Knp\Bundle\MenuBundle\KnpMenuBundle(),
             new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
             new Sonata\AdminBundle\SonataAdminBundle(),
+            new Sonata\EasyExtendsBundle\SonataEasyExtendsBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
